df1736ffa3e26e040add4d67d5c9f42d6d30dff0
zito <>
2022-12-08 14:15:58 +0800

    Learn node.js

---
+++ app.js
@@ -0,0 +1,5 @@
+let try2 = require("./try2");
+
+let myName = "zito chen";
+
+try2.night(myName);
---
+++ try2.js
@@ -0,0 +1,5 @@
+function night(name) {
+  console.log("Good night, " + name);
+}
+
+module.exports.night = night;
